54198ec2593cb87322182d49177e3ee78e120535
John Palevich <john.palevich@gmail.com>
2019-10-10 14:55:07 -0700

    Update
    Moved entire project into this folder repository.
    -Started creating a new enemy, which is a shooter.

---
+++ Reincarnator/Assets/Scripts/ShooterController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterController : MonoBehaviour
+{
+    public int cooldown;
+    public GameObject player;
+
+
+    private float timeStamp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeStamp = 0;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (timeStamp <= Time.time)
+        {
+            Shoot();   
+        }
+    }
+
+    private void Shoot()
+    {
+        timeStamp = Time.time + cooldown;
+
+    }
+}
