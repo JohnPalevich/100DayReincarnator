69ad7462615af4a3234a7bd5d198e5d273734d6f
John Palevich <john.palevich@gmail.com>
2019-10-22 14:55:15 -0700

    Fixing the bullets
    Figured out how to delete the bullets after they collide. I was using the wrong, method, I was using OnCollisionEnter2D, but I should have been using OnCollisionStay2D.  Also turnsout I had an error in sending the bullets towards the player. It was shooting only towards 0,0 because the player object the bullets were referencing the Player prefab's position. To fix this, I removed the Player prefab totally, and moved all my code in the bullet's start to in the shoot method of the shootercontroller. It is working for when the players position has a positive y, but it isn't working with a negative y. I dont know why yet.

--- Reincarnator/Assets/Scripts/BulletController.cs
+++ Reincarnator/Assets/Scripts/BulletController.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public GameObject player;
+   
     public int speed;
 
-    private Rigidbody2D rb2d;
+    //private GameObject player;
+    //private Rigidbody2D rb2d;
     void Start()
     {
-        //point the projectile towards the enemy.
-        Vector3 diff = player.transform.position - transform.position;
-        diff.Normalize();
-
-        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
-
-        //Initialized bullet's rigidbody, and gives it a direction to move in.
-        rb2d = GetComponent<Rigidbody2D>();
-        Vector2 playerPos = player.transform.position;
-        float dirX = (transform.position.x - playerPos.x) * -1;
-        float dirY = (transform.position.y - playerPos.y) * -1;
-        Vector2 movement = new Vector2(dirX, dirY);
-        movement.Normalize();
-        rb2d.AddForce(movement * speed);
+         
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+
     }
 }
--- Reincarnator/Assets/Scripts/ShooterController.cs
+++ Reincarnator/Assets/Scripts/ShooterController.cs
@@ -7,6 +7,8 @@ public class ShooterController : MonoBehaviour
     public int cooldown;
     public GameObject player;
     public GameObject bullet;
+    public int bulletSpeed;
+
 
     private float timeStamp;
     private Rigidbody2D rb2d;
@@ -30,7 +32,31 @@ public class ShooterController : MonoBehaviour
     {
         timeStamp = Time.time + cooldown;
         Vector3 pos = new Vector3(5, 0);
-        var myNewBullet = Instantiate(bullet, pos, transform.rotation);
+
+        CreateBullet(pos);
+    }
+
+    private void CreateBullet(Vector3 pos)
+    {
+
+
+/*        //Points the projectile towards the player object.
+        Vector3 diff = player.transform.position - transform.position;
+        diff.Normalize();
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        Quaternion.Euler(0f, 0f, rot_z);*/
+
+        var myNewBullet = Instantiate(bullet);
         myNewBullet.transform.parent = gameObject.transform;
+
+
+        //Initialized bullet's rigidbody, and gives it a direction to move in.
+        Rigidbody2D rb2d = myNewBullet.GetComponent<Rigidbody2D>();
+        Vector2 playerPos = player.transform.position;
+        float dirX = (transform.position.x - playerPos.x) * -1;
+        float dirY = (transform.position.y - playerPos.y) * -1;
+        Vector2 movement = new Vector2(dirX, dirY);
+        movement.Normalize();
+        rb2d.AddForce(movement * 100 * bulletSpeed);
     }
 }
