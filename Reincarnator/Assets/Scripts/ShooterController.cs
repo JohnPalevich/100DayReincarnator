5ae39e2196a1079c69c09bc138fb8668b9bb39d3
John Palevich <john.palevich@gmail.com>
2019-10-24 19:06:30 -0700

    Fixed Most things about shooter
    I fixed the error in the shooter which was causing the bullets to be missing if the player was not in the quadrant to the upper left of the enemy. This was caused by a copy and paste mistake, rather than comparing player and bullet's position, I was comparing bullet and shooter's position. Added code so that the shooter retreats after the player gets close to it.  Also added locations for shooters to shoot bullets from, from their upper lower left and right hand corners

--- Reincarnator/Assets/Scripts/ShooterController.cs
+++ Reincarnator/Assets/Scripts/ShooterController.cs
@@ -8,7 +8,8 @@ public class ShooterController : MonoBehaviour
     public GameObject player;
     public GameObject bullet;
     public int bulletSpeed;
-
+    public int speed;
+    public float increment;
 
     private float timeStamp;
     private Rigidbody2D rb2d;
@@ -26,6 +27,12 @@ public class ShooterController : MonoBehaviour
         {
             Shoot();   
         }
+        Vector2 dist = DistToPlayer(transform.position);
+        if (dist.magnitude < 3)
+        {
+            dist.Normalize();
+            rb2d.AddForce(dist * 70 * -1);
+        }
     }
 
     private void Shoot()
@@ -33,30 +40,50 @@ public class ShooterController : MonoBehaviour
         timeStamp = Time.time + cooldown;
         Vector3 pos = new Vector3(5, 0);
 
-        CreateBullet(pos);
+        CreateBullet(ShootPosition());
     }
 
     private void CreateBullet(Vector3 pos)
     {
-
-
-/*        //Points the projectile towards the player object.
-        Vector3 diff = player.transform.position - transform.position;
+        //Points the projectile towards the player object.
+        Vector3 diff = player.transform.position - pos;
         diff.Normalize();
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-        Quaternion.Euler(0f, 0f, rot_z);*/
 
-        var myNewBullet = Instantiate(bullet);
+        var myNewBullet = Instantiate(bullet, pos, Quaternion.Euler(0f, 0f, rot_z - 12));
         myNewBullet.transform.parent = gameObject.transform;
-
 
         //Initialized bullet's rigidbody, and gives it a direction to move in.
         Rigidbody2D rb2d = myNewBullet.GetComponent<Rigidbody2D>();
-        Vector2 playerPos = player.transform.position;
-        float dirX = (transform.position.x - playerPos.x) * -1;
-        float dirY = (transform.position.y - playerPos.y) * -1;
-        Vector2 movement = new Vector2(dirX, dirY);
+        Vector2 movement = DistToPlayer(pos);
         movement.Normalize();
         rb2d.AddForce(movement * 100 * bulletSpeed);
+    }
+
+    private Vector2 DistToPlayer(Vector3 pos)
+    {
+        Vector2 playerPos = player.transform.position;
+        float dirX = (pos.x - playerPos.x) * -1;
+        float dirY = (pos.y - playerPos.y) * -1;
+        Vector2 vec = new Vector2(dirX, dirY);
+        return vec;
+    }
+
+    private Vector3 ShootPosition()
+    {
+        Vector2 dist = DistToPlayer(transform.position);
+        if(dist.x > 0 && dist.y > 0)
+        {
+            return new Vector3(transform.position.x + increment, transform.position.y + increment);
+        }
+        else if(dist.x > 0 && dist.y < 0)
+        {
+            return new Vector3(transform.position.x + increment, transform.position.y - increment);
+        }
+        else if (dist.x < 0 && dist.y < 0)
+        {
+            return new Vector3(transform.position.x - increment, transform.position.y - increment);
+        }
+        return new Vector3(transform.position.x - increment, transform.position.y + increment);
     }
 }
