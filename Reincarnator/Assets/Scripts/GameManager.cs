bc07feeb85e10b4712951bf6debf366c03b7e185
John Palevich <john.palevich@gmail.com>
2019-10-29 14:59:57 -0700

    +Random Level Generation
    -Organized prefabs
    -Finished my random level generation prefab and tested it out. Game no longer works b/c no player object, but enemies and levels are randomly generated now.
    -Created a bunch of new prefabs: TilePrefabs, TextPrefabs

--- Reincarnator/Assets/Scripts/GameManager.cs
+++ Reincarnator/Assets/Scripts/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoardManager boardScript;
+
+    private int level = 3;
+    //https://learn.unity.com/tutorial/level-generation?projectId=5c514a00edbc2a0020694718#5c7f8528edbc2a002053b6f7 4:00 left off
+    void Awake()
     {
-        
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetUpScene(level);
     }
 
     // Update is called once per frame
